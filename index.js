e2cc3a8a4fa27c5f10de7c738c8233dff73bce29
April Copes <antdevelopment1@gmail.com>
2018-10-19 21:23:36 -0400

    Adds types of common runtime complexities

---
+++ index.js
@@ -0,0 +1,13 @@
+// ==============================================
+// Different common types of runtime complexities
+// ==============================================
+// -Constant evaluates to: 1
+// -Logarithmic evaluates to: log(n)
+// -Linear evaluates to: n
+// -Quasilinear evaluates to: n * log(n)
+// -Quadratic evaluates to: n ^ 2
+// -Exponential evaluates to: 2 ^ n
+
+// ==================================================================================
+// Determining The Runtime Complexity of a Function, ALgorithm, or and Code Execution
+// ==================================================================================
