68ecfda0d2cfafef26fb3097ffbe7df200ee59ea
April Copes <antdevelopment1@gmail.com>
2018-10-19 23:34:26 -0400

    Solves another run time complexity for function

--- index.js
+++ index.js
@@ -109,5 +109,20 @@
 //     }
 // }
 
-// Answer 3
+// Answer 3:
+// O(n) linear
+
+// Question 4:
+// Determine the time complexity for the following function
+
+// function onlyElementsAtEvenIndex(array) {
+//     var newArray = Array(Math.ceil(array.length / 2));
+//     for (var i = 0; i < array.length; i++) {
+//         if (i % 2 === 0) {
+//             newArray[i / 2] = array[i];
+//         }
+//     }
+//     return newArray;
+// }
+// Answer 4:
 // O(n) linear
