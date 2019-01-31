9b5cc698950b8b845a32f390faa7af4fdb58e513
April Copes <antdevelopment1@gmail.com>
2019-01-30 20:59:47 -0500

    Adds step two for solving new problems

--- problemSolving.js
+++ problemSolving.js
@@ -1,18 +1,28 @@
 // Techniques to solving problems
 // Write a function that takes two numbers and returns their sum
-
-// First Step
+// =================================
+// Understanding the Problem Step 1:
+// =================================
 // Can I restate the problem into my won words
 
-
-// Second Step
 // What are the input that go into the problem
 
-// Third Step
 // What are the outputs that should come from the solution of the problem
 
-// Fourth Step
 // Can the outputs be determined from the inputs? In other words do I have enough infornmation to solve thr problem
 
-// Fifth Step
 // How should I label the important pieces of data that are part of the problem
+
+// ==========================
+// Concrete Examples Step 2:
+// ==========================
+
+// Write a function that takes a string and returns counts of each character in the string
+
+// Start with simple examples
+
+// Progress to more complex examples
+
+// Explore examples with empty inputs
+
+// Explore examples with invalid inputs
