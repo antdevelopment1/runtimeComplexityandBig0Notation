f2d092f3ba901e88eb48ef117bc395ec8d1c2a4b
April Copes <antdevelopment1@gmail.com>
2019-01-30 20:50:14 -0500

    updated

--- problemSolving.js
+++ problemSolving.js
@@ -1,2 +1,18 @@
 // Techniques to solving problems
 // Write a function that takes two numbers and returns their sum
+
+// First Step
+// Can I restate the problem into my won words
+
+
+// Second Step
+// What are the input that go into the problem
+
+// Third Step
+// What are the outputs that should come from the solution of the problem
+
+// Fourth Step
+// Can the outputs be determined from the inputs? In other words do I have enough infornmation to solve thr problem
+
+// Fifth Step
+// How should I label the important pieces of data that are part of the problem
