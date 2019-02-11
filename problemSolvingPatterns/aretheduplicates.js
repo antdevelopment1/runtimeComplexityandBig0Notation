b8f1d44fc5c9f2feb9a22e2c7017eb4e88e53e3e
April Copes <antdevelopment1@gmail.com>
2019-02-10 21:31:52 -0500

    Adds new way to solve challange

--- problemSolvingPatterns/aretheduplicates.js
+++ problemSolvingPatterns/aretheduplicates.js
@@ -4,11 +4,14 @@
 function areThereDuplicates() {
     // We initialize an empty object
     let collection = {};
+    console.log(collection);
 
     // We loop through the number of arguments passed in when we invoke the functions by using the arguments object created by javascript
     for (let val in arguments) {
         // We save the value for each key to equal +1 if it is already inside the object or set it to 0 plus one if is not inside the object
         collection[arguments[val]] = (collection[arguments[val]] || 0) + 1;
+        
+        console.log('z');
     }
 
     // We loop through each key in the collection
@@ -50,5 +53,5 @@ function areThereDuplicates(...args) {
     return false;
 }
 
-console.log(areThereDuplicates(1,2,5,6,7,7));
+// console.log(areThereDuplicates(1,2,5,6,7,7));
 
