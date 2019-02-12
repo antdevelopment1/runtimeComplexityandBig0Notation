40221785c547b69a3bac763f315b28db1424365a
April Copes <antdevelopment1@gmail.com>
2019-02-11 21:43:00 -0500

    Adds solution to algorithm to check if a group of numbers have the largest sum in a given array

--- problemSolvingPatterns/slidingWindow.js
+++ problemSolvingPatterns/slidingWindow.js
@@ -21,3 +21,30 @@ function maxSubArraySum(arr, num) {
 }
 
 console.log(maxSubArraySum([2,6,9,2,1,8,5,6,3], 3));
+
+function maxSubarraySum(arr, num){
+    // If the length of the array is less than the number passed we will return null because all conditions will be all fail if we can't check anything else
+    if (arr.length < num) return null;
+    
+    // We set the total to 0
+    let total = 0;
+
+    // We loop n number of times and accumlicate the index values and save them in total value
+    for (let i=0; i<num; i++){
+       total += arr[i];
+    }
+    // We temporarily set the current toal to the collected total from our first for loop
+    let currentTotal = total;
+
+    // We set i equal to the number passed and run the loop starting from that the number passed until it is greater thsn the length of the array
+    for (let i = num; i < arr.length; i++) {
+       
+        // we add the value of the next seated number in the array and substract the previous number seated in the array
+       currentTotal += arr[i] - arr[i-num];
+        // Then we check the max values between the total and current total and set it equal to total using a built in JavaScript method
+       total = Math.max(total, currentTotal);
+    }
+
+    // We return the total
+    return total;
+}
