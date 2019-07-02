47f74f94c1b021a66d1a1631b063db6083b9dee6
April Copes <antdevelopment1@gmail.com>
2019-07-01 23:18:13 -0400

    practice old algorithm example

--- problemSolvingPatterns/excersise1.js
+++ problemSolvingPatterns/excersise1.js
@@ -21,44 +21,40 @@
 
 // same([1,2,3,4], [1,4,9,16]);
 
-
 // Refactored Code O(N) Linear as opposed to the quadratic one above Runtime Complexity
-function same(arr1, arr2) {
+// function same(arr1, arr2) {
+//   // We check to see
+//   if (arr1.length !== arr1.length) {
+//     return false;
+//   }
 
-    // We check to see 
-    if (arr1.length !== arr1.length) {
-        return false;
-    }
+//   let frequencyCounter1 = {};
+//   let frequencyCounter2 = {};
 
-    let frequencyCounter1 = {};
-    let frequencyCounter2 = {};
+//   for (let val of arr1) {
+//     // Set equal to itself  plus one if it already exsists or booleans to 0 and increments to 1
+//     frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1;
+//   }
 
-    for (let val of arr1) {
-            // Set equal to itself  plus one if it already exsists or booleans to 0 and increments to 1
-        frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1;
-    }
+//   for (let val of arr2) {
+//     frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1;
+//   }
 
-    for (let val of arr2) {
-        frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1;
-    }
+//   for (let key in frequencyCounter1) {
+//     if (!(key ** 2 in frequencyCounter2)) {
+//       return false;
+//     }
+//     if (frequencyCounter2[key ** 2] !== frequencyCounter1[key]) {
+//       return false;
+//     }
+//   }
 
-    for (let key in frequencyCounter1) {
-        if (!(key ** 2 in frequencyCounter2)) {
-            return false;
-        }
-        if (frequencyCounter2[key**2] !== frequencyCounter1[key]) {
-            return false;
-        }
-    }
+//   // console.log(frequencyCounter1);
+//   // console.log(frequencyCounter2);
+//   return true;
+// }
 
-    // console.log(frequencyCounter1);
-    // console.log(frequencyCounter2);
-    return true;
-
-}
-
-console.log(same([1,2,3,4], [1,4,9,16]));
-
+// console.log(same([1, 2, 3, 4], [1, 4, 9, 16]));
 
 // Given two positive integers write a function to find out if two numbers have the same frequency of digits
 // The solution must have the folowing complexities - Time O(N)
@@ -68,16 +64,14 @@ console.log(same([1,2,3,4], [1,4,9,16]));
 //     // Convert to strings
 //     let number1 = int1.toString();
 //     let number2 = int2.toString();
-    
 
 // //    Evalue
 //     if (int1.length !== int2.length) {
 //         return false;
 //     }
-    
-    
+
 //     const lookup = {};
-    
+
 //     for (let i = 0; i < number1.length; i++) {
 //         let digit = number1[i];
 //         if (lookup[[digit]]) {
@@ -97,7 +91,39 @@ console.log(same([1,2,3,4], [1,4,9,16]));
 //     }
 
 //     return true;
-    
+
 //   }
 
 //   console.log(sameFrequency(123, 231));
+
+function sameFrequency(arr1, arr2) {
+  let getLengthOne = arr1.toString();
+  let getLengthTwo = arr2.toString();
+
+  if (getLengthOne.length !== getLengthTwo.length) {
+    return false;
+  }
+
+  const lookup = {};
+
+  for (let i = 0; i < getLengthOne.length; i++) {
+    let digit = getLengthOne[i];
+    if (lookup[digit]) {
+      lookup[digit] += 1;
+    } else {
+      lookup[digit] = 1;
+    }
+  }
+
+  for (let i = 0; i < getLengthTwo.length; i++) {
+    let digit = getLengthTwo[i];
+    if (!lookup[digit]) {
+      return false;
+    } else {
+      lookup[digit] -= 1;
+    }
+  }
+  return true;
+}
+
+console.log(sameFrequency(123, 312));
